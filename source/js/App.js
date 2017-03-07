2ac45af8f6fd3785aa9c8b2482d9bde87c8eb106
Robin <info@devvv.se>
2017-03-07 16:20:37 +0100

    Started working on AJAX call

--- source/js/App.js
+++ source/js/App.js
@@ -1,1 +1,30 @@
-var Knowit = Knowit || {};
+$(function(){ 
+		//Get list on document ready. Change later to 'if search input empty GET list'.
+
+		 $.getJSON("products.php", function(data){ 
+            /* loop through array */
+           	$.each(data, function(index, d){            
+                $('<tr />').append(
+               	$('<td />').text(d.name),
+               	$('<td />').text(d.group),
+               	$('<td />').html("<a href='"+d.url+"'>"+'Read more'+"</a>")
+               	).appendTo('.search-table');
+            });
+        });
+
+		 //When search input is changed, SEND new search query and GET new result
+
+		 $('input[type=search]').keyup(function(){
+		    var value = $( this ).val();
+		 	
+		 	$.getJSON("products.php", { s:value }, function(data){
+			 $.each(data, function(index, d){            
+                $('<tr />').replaceAll(
+               	$('<td />').text(d.name),
+               	$('<td />').text(d.group),
+               	$('<td />').html("<a href='"+d.url+"'>"+'Read more'+"</a>")
+               	).appendTo('.search-table');
+            });	
+	 	});
+	});
+});
