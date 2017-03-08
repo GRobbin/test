ae4c92aac4faaef7ac625c095525b574fde3a169
Robin <info@devvv.se>
2017-03-08 09:57:25 +0100

    added search loop

--- source/php/Products.php
+++ source/php/Products.php
@@ -4,14 +4,18 @@ namespace KnowitToolsTest;
 
 class Products
 {
+
     /**
      * Get all products
      * @return array
      */
+
+
     public static function all() : array
     {
         return json_decode(file_get_contents(PRODUCTS_JSON));
     }
+
 
     /**
      * Search products
@@ -20,30 +24,40 @@ class Products
      */
     public static function search($q)
     {
-        $search = new \KnowitToolsTest\SearchEngine(
-            self::all(),
-            $q,
-            array(
-                'name'
-            )
-        );
+        $search = new \KnowitToolsTest\SearchEngine(self::all(), $q, array('name'));
 
-        return $search->result;
+        var_dump($search->result);
     }
 }
 class SearchEngine
-{
+
     /**
-     * Search algoritm
+     *  
+     *
      * @param  
-     * @return 
+     * @return
      */
 
+{
     public $result = '';
 
     public function __construct($p, $q, $arr)
-    {
-            $x = explode(' ', $q);
-            $this->result = $x;
+    { 
+        
 
+            foreach($p as $item)
+            {
+                $match = $item->name;
+
+                if(preg_match("/\b$q\b/i", $match))
+                {
+                    print_r($item);
+
+                }
+
+            }
+
+                
     }
+    
+}
