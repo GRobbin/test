42642791350241d60e867ef442ded6a3b5bfebf6
Robin <info@devvv.se>
2017-03-12 22:55:53 +0100

    added live search function

--- source/js/App.js
+++ source/js/App.js
@@ -1,30 +1,27 @@
 $(function(){ 
-		//Get list on document ready. Change later to 'if search input empty GET list'.
-
-		 $.getJSON("products.php", function(data){ 
+  //jQuery code here 
+     $.getJSON("products.php", function(data){ 
             /* loop through array */
-           	$.each(data, function(index, d){            
+            $.each(data, function(index, d){            
                 $('<tr />').append(
-               	$('<td />').text(d.name),
-               	$('<td />').text(d.group),
-               	$('<td />').html("<a href='"+d.url+"'>"+'Read more'+"</a>")
-               	).appendTo('.search-table');
+                $('<td />').text(d.name),
+                $('<td />').text(d.group),
+                $('<td />').html("<a href='"+d.url+"'>"+'Read more'+"</a>")
+                ).appendTo('.load');
             });
         });
-
-		 //When search input is changed, SEND new search query and GET new result
-
-		 $('input[type=search]').keyup(function(){
-		    var value = $( this ).val();
-		 	
-		 	$.getJSON("products.php", { s:value }, function(data){
-			 $.each(data, function(index, d){            
-                $('<tr />').replaceAll(
-               	$('<td />').text(d.name),
-               	$('<td />').text(d.group),
-               	$('<td />').html("<a href='"+d.url+"'>"+'Read more'+"</a>")
-               	).appendTo('.search-table');
-            });	
-	 	});
-	});
+     /* loop through array on search */
+     $('input[type=search]').keyup(function(){
+        $('.load').empty();
+        var value = $( this ).val();
+      $.getJSON("products.php", { s:value }, function(data){
+       $.each(data, function(index, d){            
+                $('<tr />').append(
+                $('<td />').text(d.name),
+                $('<td />').text(d.group),
+                $('<td />').html("<a href='"+d.url+"'>"+'Read more'+"</a>")
+                ).appendTo('.load');
+            }); 
+    });
+  });
 });
