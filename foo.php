485231c32f39ed898dacd613c199358ba3306460
Robin <info@devvv.se>
2017-03-14 16:31:52 +0100

    deleted test file

--- foo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
-
-require_once 'source/php/App.php';
-$products = \KnowitToolsTest\Products::search('digital');
-\KnowitToolsTest\Response::json($products);
-
